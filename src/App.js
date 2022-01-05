4b4100d6174c68929e3594127b8d8fba20cce18a
Ronni Hartlage <hartlage.7@wright.edu>
2022-01-05 10:59:24 -0500

    removed some unnecessary code/files autogenerated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-// npm start to run
-// import logo from './logo.svg';
 import './App.css';
-import Route from "react-router-dom";
-import Login from "./containers/Login";
+// import Route from "react-router-dom";
+// import Login from "./containers/Login";
 
 function App() {
   return (
@@ -12,9 +10,9 @@ function App() {
 
     <div className="App">
 
-      <Route exact path="/login">
+      {/* <Route exact path="/login">
         <Login />
-      </Route>
+      </Route> */}
 
       <h1> Medication Delivery Robot </h1>
 
@@ -23,26 +21,7 @@ function App() {
       <a href="/" className="btn"> Room 2 </a>
       <a href="/" className="btn"> Room 3 </a>
     </div>
-
-    // <div className="App">
-    //   <h1> Medication Delivery Robot </h1>
-    //   {/* <a href="/">Main Page</a> */}
-
-    //   {/* <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header> */}
-    // </div>
+    
   );
 }
 
