1d59a781dbe6c2fc132848b7104a1dad5197137d
Ronni Hartlage <hartlage.7@wright.edu>
2022-01-05 09:47:38 -0500

    started web application using React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+// npm start to run
+// import logo from './logo.svg';
 import './App.css';
+import Route from "react-router-dom";
+import Login from "./containers/Login";
 
 function App() {
   return (
+
+    // make password screen 
+    // https://www.digitalocean.com/community/tutorials/how-to-add-login-authentication-to-react-applications
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Route exact path="/login">
+        <Login />
+      </Route>
+
+      <h1> Medication Delivery Robot </h1>
+
+      <h2> Choose Delivery Destination </h2>
+      <a href="/" className="btn"> Room 1 </a>
+      <a href="/" className="btn"> Room 2 </a>
+      <a href="/" className="btn"> Room 3 </a>
     </div>
+
+    // <div className="App">
+    //   <h1> Medication Delivery Robot </h1>
+    //   {/* <a href="/">Main Page</a> */}
+
+    //   {/* <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header> */}
+    // </div>
   );
 }
 
