4d0b1ebdaa7270b1414565f69382446bc3b4f5b7
BDDave-Student <70174975+BDDave-Student@users.noreply.github.com>
2022-02-26 04:25:04 -0500

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -1,6 +1,7 @@
 //  Create Axios object to interface with Sequelize ORM
 import axios from "axios";
-
+//  localhost is an issue for outside devices to
+//  connect with the server to pull data
 export default axios.create({
   baseURL: "http://localhost:3001/api",
   headers: {
